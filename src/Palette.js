9d4f43b4d63ede713f19c517cbebfa88f0c2fb3a
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2020-10-28 11:31:53 -0500

    First commit

---
+++ src/Palette.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+
+    render() {
+
+        return (
+            <div className='Palette' >
+                {/* NavBar here */}
+                <div className='Palette-colors' >
+                    {/* Our color boxes */}
+                </div>
+                {/* Footer */}
+            </div>
+        );
+    }
+}
+
+export default Palette;
