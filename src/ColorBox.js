69f9a7b7169ea524d985481685127d4e9981f37a
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2020-10-29 11:13:25 -0500

    Added Copy to clipBoard NPM package

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,26 +1,49 @@
 import React, { Component } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './ColorBox.css'
 
 class ColorBox extends Component {
 
     constructor(props) {
         super(props)
+
+        this.state = {
+            value: '',
+            copied: false
+        }
+
+        // this.onCopy = this.onCopy.bind(this)
+
     }
 
+    // onCopy() {
+    //     this.setState({ value: this.props.boxColor.color, copied: true })
+    // }
+
+
     render() {
+
+        console.log(this.state);
+
         const { name, color } = this.props.boxColor
         return (
-            <div style={{ backgroundColor: `${color}` }} className='ColorBox' >
-                <div className='copy-container' >
-                    <div className='box-content' >
-                        <span>{name}</span>
+            <CopyToClipboard text={`${color}`}>
+                <div style={{ backgroundColor: `${color}` }} className='ColorBox' >
+                    <div className='copy-container' >
+                        <div className='box-content' >
+                            <span>{name}</span>
+                        </div>
+                        <button className='copy-button'  >Copy</button>
                     </div>
-                    <button className='copy-button' >Copy</button>
+                    <span className='see-more' >More</span>
                 </div>
-                <span className='see-more' >More</span>
-            </div>
+            </CopyToClipboard>
         );
     }
 }
 
-export default ColorBox; <h2>Color BOx Component</h2>
+export default ColorBox;
+
+
+
+// onClick={this.onCopy}
