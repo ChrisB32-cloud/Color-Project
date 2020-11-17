ae9b6e65eeced75d86234c3a45cddd384153c6ad
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2020-11-17 12:29:31 -0600

    Refactored withStyles ColorBox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -58,6 +58,67 @@ const styles = {
         border: 'none',
         textDecoration: 'none',
         opacity: '0'
+    },
+    boxContent: {
+        position: 'absolute',
+        width: '100%',
+        left: '0px',
+        bottom: '0px',
+        padding: '10px',
+        color: 'black',
+        letterSpacing: '1px',
+        textTransform: 'uppercase',
+        fontSize: '12px',
+    },
+    copyOverlay: {
+        opacity: '0',
+        zIndex: '0',
+        width: '100%',
+        height: '100%',
+        transition: 'transform 0.6s ease-in-out',
+        transform: 'scale(0.1)',
+    },
+    showOverlay: {
+        opacity: '1',
+        transform: 'scale(50)',
+        zIndex: '10',
+        position: 'absolute',
+    },
+    copyMsg: {
+        position: 'fixed',
+        top: '0',
+        left: '0',
+        right: '0',
+        bottom: '0',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '4rem',
+        transform: 'scale(0.1)',
+        color: 'white',
+        opacity: '0',
+        "& h1": {
+            fontWeight: '400',
+            textShadow: '1px 2px black',
+            background: 'rgba(255, 255, 255, 0.3)',
+            width: '100%',
+            textAlign: 'center',
+            marginBottom: '0',
+            padding: '1rem',
+            textTransform: 'uppercase',
+        },
+        "& p": {
+            fontSize: '2rem',
+            fontWeight: '100',
+        }
+    },
+    showCopyMsg: {
+        opacity: '1',
+        transform: 'scale(1)',
+        zIndex: '25',
+        transition: 'all 0.4s ease-in-out',
+        transitionDelay: '0.3s',
     }
 }
 
@@ -86,13 +147,13 @@ class ColorBox extends Component {
         return (
             <CopyToClipboard text={`${boxColor}`} onCopy={this.changeCopyState} >
                 <div style={{ backgroundColor: `${boxColor}` }} className={classes.ColorBox} >
-                    <div style={{ backgroundColor: `${boxColor}` }} className={`copy-overlay ${this.state.copied && "show"}`} />
-                    <div className={`copy-msg ${this.state.copied && 'show'}`} >
-                        <h1>Copied!</h1>
-                        <p className={classes.copyText}  >{boxColor}</p>
+                    <div style={{ backgroundColor: `${boxColor}` }} className={`${classes.copyOverlay} ${this.state.copied && classes.showOverlay}`} />
+                    <div className={`${classes.copyMsg} ${this.state.copied && classes.showCopyMsg}`} >
+                        <h1 >Copied!</h1>
+                        <p   >{boxColor}</p>
                     </div>
-                    <div className='copy-container' >
-                        <div className='box-content' >
+                    <div  >
+                        <div className={classes.boxContent} >
                             <span className={classes.colorName}>{name}</span>
                         </div>
                         <button className={classes.copyButton}  >Copy</button>
