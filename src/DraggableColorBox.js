12f9b3a5de317ae8188d6adcaa644436f1cd407a
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2020-12-03 11:09:26 -0600

    Added position on the draggable color content and a delete icon

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,33 +1,57 @@
 import React from 'react';
+import DeleteIcon from '@material-ui/icons/Delete';
 import { withStyles } from '@material-ui/styles';
 
-
 const styles = {
-
-    showingNewBoxes: {
-        width: '20%',
-        height: '25%',
-        margin: '0 auto',
-        display: 'inline-block',
-        position: 'relative',
-        cursor: 'pointer',
-        marginBottom: '-3.5px',
+  showingNewBoxes: {
+    width: '20%',
+    height: '25%',
+    margin: '0 auto',
+    display: 'inline-block',
+    position: 'relative',
+    cursor: 'pointer',
+    marginBottom: '-3.5px',
+    '&:hover svg': {
+      color: 'white',
+      transform: 'scale(1.5)'
     }
-}
+  },
+  boxContent: {
+    position: 'absolute',
+    width: '100%',
+    left: '0px',
+    bottom: '0px',
+    padding: '10px',
+    color: 'rgba(0,0,0,0.5)',
+    letterSpacing: '1px',
+    textTransform: 'uppercase',
+    fontSize: '12px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    lineHeight: '30px'
+  },
+  delete: {
+    transition: 'all 0.3s ease-in-out'
+  }
+};
 
-const DraggableColorBox = (props) => {
-    // console.log(props);
-    const { classes } = props
-    return (
-        <div className={classes.showingNewBoxes} style={{ backgroundColor: props.color }}>
-            {props.theName}
-        </div>
-    );
+const DraggableColorBox = props => {
+  // console.log(props);
+  const { classes } = props;
+  return (
+    <div
+      className={classes.showingNewBoxes}
+      style={{ backgroundColor: props.color }}
+    >
+      <div className={classes.boxContent}>
+        <span>{props.theName}</span>
+        <DeleteIcon className={classes.delete} />
+      </div>
+    </div>
+  );
 };
 
 export default withStyles(styles)(DraggableColorBox);
-
-
 
 // const [state, setState] = useState({
 //     currentColor: 'teal',
