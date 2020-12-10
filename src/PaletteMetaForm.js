6a44cf29d7b6203ea526295e30080b401d62a69d
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2020-12-10 12:04:20 -0600

    New file PaletteMetalForm.js for our popup input (modal)

---
+++ src/PaletteMetaForm.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const PaletteMetaForm = () => {
+  return (
+    <div>
+      <h1>Meta Form Component</h1>
+    </div>
+  );
+};
+
+export default PaletteMetaForm;
