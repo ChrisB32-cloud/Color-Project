750eead26af6db8d80c86104501e77a1549cdeb6
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2020-12-12 11:03:29 -0600

    Updated styles on paletteList and miniPalette

--- src/styles/MiniPaletteStyles.js
+++ src/styles/MiniPaletteStyles.js
@@ -1,45 +1,45 @@
 export default {
-    root: {
-        backgroundColor: 'white',
-        border: '1px solid black',
-        borderRadius: '5px',
-        padding: '0.5rem',
-        position: 'relative',
-        overflow: 'hidden',
-        "&:hover": {
-            cursor: 'pointer'
-        }
-    },
-    colors: {
-        backgroundColor: '#DAE1E4',
-        height: '150px',
-        width: '100%',
-        borderRadius: '5px',
-        overflow: 'hidden'
-
-    },
-    miniBox: {
-        height: '25%',
-        width: '20%',
-        display: 'inline-block',
-        margin: '0 auto',
-        position: 'relative',
-        marginBottom: '-3.5px'
-    },
-    title: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginTop: '-0.2rem',
-        marginBottom: '1.5rem',
-        color: 'black',
-        paddingTop: '0.5rem',
-        fontSize: '1rem',
-        position: 'relative'
-
-    },
-    emoji: {
-        marginLeft: '0.5rem',
-        fontSize: '1.5rem'
+  root: {
+    backgroundColor: 'white',
+    border: '1px solid black',
+    // maxHeight: '200px',
+    borderRadius: '5px',
+    padding: '0.5rem',
+    position: 'relative',
+    overflow: 'hidden',
+    '&:hover': {
+      cursor: 'pointer'
     }
-}
+  },
+  colors: {
+    backgroundColor: '#DAE1E4',
+    height: '150px',
+    width: '100%',
+    borderRadius: '5px',
+    overflow: 'hidden'
+  },
+  miniBox: {
+    height: '25%',
+    width: '20%',
+    display: 'inline-block',
+    margin: '0 auto',
+    position: 'relative',
+    marginBottom: '-3.5px'
+  },
+  title: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: '0',
+    // marginTop: '-0.2rem',
+    // marginBottom: '1.5rem',
+    color: 'black',
+    paddingTop: '0.5rem',
+    fontSize: '1rem',
+    position: 'relative'
+  },
+  emoji: {
+    marginLeft: '0.5rem',
+    fontSize: '1.5rem'
+  }
+};
--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,37 +1,38 @@
 export default {
-    root: {
-        backgroundColor: 'blue',
-        display: 'flex',
-        height: '150vh',
-        alignItmes: 'flex-start',
-        justifyContent: 'center'
+  root: {
+    backgroundColor: 'blue',
+    display: 'flex',
+    height: '100vh',
+    alignItmes: 'flex-start',
+    justifyContent: 'center',
+    overflow: 'scroll'
+  },
+  container: {
+    width: '50%',
+    // height: '100vh',
+    // maxHeight: '180px',
+    display: 'flex',
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    flexWrap: 'wrap'
 
-    },
-    container: {
-        width: '50%',
-        // height: '100vh',
-        display: 'flex',
-        alignItems: 'flex-start',
-        flexDirection: 'column',
-        flexWrap: 'wrap',
-        // overflow: 'scroll'
-        // border: '1px solid white'
-    },
-    nav: {
-        display: 'flex',
-        width: '100%',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        color: 'white',
-        "& a": {
-            color: 'white'
-        }
-    },
-    palettes: {
-        boxSizing: 'border-box',
-        width: '100%',
-        display: 'grid',
-        gridTemplateColumns: 'repeat(3, 30%)',
-        gridGap: '5%'
+    // border: '1px solid white'
+  },
+  nav: {
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    color: 'white',
+    '& a': {
+      color: 'white'
     }
-}
+  },
+  palettes: {
+    boxSizing: 'border-box',
+    width: '100%',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 30%)',
+    gridGap: '5%'
+  }
+};
